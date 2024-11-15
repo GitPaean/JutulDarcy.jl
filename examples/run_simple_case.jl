0d827255bda5cd926e4a4513d007fe6ca6f665e1
Kai Bao <kai.bao@sintef.no>
2024-11-15 10:57:07 +0100

    some modification to the case.

--- examples/run_simple_case.jl
+++ examples/run_simple_case.jl
@@ -1,11 +1,27 @@
 using JutulDarcy
 using Jutul
 using GLMakie
-## run the simulation
-# data_path = joinpath(@__DIR__, "data/SIMPLE_COMP.DATA")
-dpth = JutulDarcy.GeoEnergyIO.test_input_file_path("SIMPLE_COMP")
-data_path = joinpath(dpth, "SIMPLE_COMP.DATA")
+using Logging
+# using PrettyPrint:pretty_print
+# import PrettyPrint
+
+# # global_logger(Logging.ConsoleLogger(stderr, Logging.Debug));
+
+# run the simulation
+data_path = joinpath(@__DIR__, "data/SIMPLE_COMP.DATA")
+# dpth = JutulDarcy.GeoEnergyIO.test_input_file_path("SIMPLE_COMP")
+# data_path = joinpath(dpth, "SIMPLE_COMP.DATA")
 case = setup_case_from_data_file(data_path)
+# println("dump usage")
+# dump(case)
+# println("dump usage end")
+# println("show")
+# @show case
+# println("show usage end")
+# println("pretty print usage end")
+# PrettyPrint.pprint(case)
+# println("pretty print usage end")
+
 result = simulate_reservoir(case, info_level = 1)
 ws, states = result;
 
