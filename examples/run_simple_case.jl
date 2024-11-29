afc0938f26bbbb04c41a9f54f556bb02ac1b8fa9
Kai Bao <kai.bao@sintef.no>
2024-11-29 16:34:46 +0100

    updating the testing case.

--- examples/run_simple_case.jl
+++ examples/run_simple_case.jl
@@ -2,13 +2,15 @@ using JutulDarcy
 using Jutul
 using GLMakie
 using Logging
+
+
 # using PrettyPrint:pretty_print
 # import PrettyPrint
 
 # # global_logger(Logging.ConsoleLogger(stderr, Logging.Debug));
 
 # run the simulation
-data_path = joinpath(@__DIR__, "data/SIMPLE_COMP.DATA")
+data_path = joinpath(@__DIR__, "data/SIMPLE_COMP_SMALL.DATA")
 # dpth = JutulDarcy.GeoEnergyIO.test_input_file_path("SIMPLE_COMP")
 # data_path = joinpath(dpth, "SIMPLE_COMP.DATA")
 case = setup_case_from_data_file(data_path)
@@ -26,4 +28,7 @@ result = simulate_reservoir(case, info_level = 1)
 ws, states = result;
 
 ## plotting the results
-plot_reservoir(case, states, step = length(case.dt), key = :Saturations)
+#plot_reservoir(case, states, step = length(case.dt), key = :Saturations);
+
+## plot the well results
+plot_well_results(ws);
